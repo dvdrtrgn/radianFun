53a4f24582bf80d93e40c61ff219446127aae93f
dvdrtrgn <dvdrtrgn@gmail.com>
2017-11-08 14:11:36 -0600

    weed out vague colors

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-/*global W, C, MOUSE, AREA, PAINT, CSS_COLORS, Mover, Runner Vector, */
+/*global W, C, MOUSE, AREA, CSS_COLORS, Draw, Mover, Runner Vector, */
 const LOOP = new Runner();
+const PAINT = new Draw('Test', AREA);
+const color = () => CSS_COLORS[rando(CSS_COLORS.length - 1)];
 const rando = (top = 9) => Math.random() * (1 + top) | 0;
-const color = () => CSS_COLORS[rando(CSS_COLORS.length)];
 
 // ---------
 // etc
@@ -18,18 +19,22 @@ function run() {
   const gravity = new Vector(0, 1);
   let movers = Array(5).fill(0);
 
-  movers = movers.map(function (e, i) {
+  movers = movers.map(function () {
+    let name = color();
     let size = rando(19) + 1;
     let mass = size / 10;
     let radius = size + 10;
-    let m = new Mover(i, mass, radius);
+    let m = new Mover(name, mass, radius);
 
     m.x = mass * AREA.w;
-    m.cf.color = color();
+    m.cf.color = name;
     m.addForce(new Vector(rando(10) - 5, rando(10) - 5));
 
     return m;
   });
+
+  // move bigger to back
+  movers.sort((a, b) => a.mass < b.mass);
 
   LOOP.init(function () {
     PAINT.clear();
